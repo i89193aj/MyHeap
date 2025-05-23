b210c5a976e6b35c2f97176e23249236e425e7a5
Oscar <i89193aj@gmail.com>
2025-05-23 15:32:00 +0800

    Heap structure Create

---
+++ MyHeap/MyHeapOutput.cpp
@@ -0,0 +1,11 @@
+//#include "pch.h";
+#include "MyHeap.h"
+#include <vector>
+#include <string>
+
+template class MyHeap<int>;
+template class MyHeap<std::string>;
+
+/*這裡先不做，給之後要丟dll的時候，還要再包一次func!*/
+
+
